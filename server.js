01aa5d19f1d10481aa110877bc9da226c3c1765c
Cao Vy <vihcce170371@fpt.edu.vn>
2024-07-10 17:57:58 +0700

    ADD:: library Express and .gitignore

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello World\n Cao Vy');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Sever running at http://${hostname}:${port}/`);
+})
