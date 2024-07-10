c861ba796bdbef60d92cc1309598902a7a5874fa
Cao Vy <vihcce170371@fpt.edu.vn>
2024-07-10 18:16:44 +0700

    UPDATE:: Hello World with Express

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World\n Cao Vy');
-});
+// const server = http.createServer((request, response) => {
+//     response.statusCode = 200;
+//     response.setHeader('Content-Type', 'text/plain');
+//     response.end('Hello World\n Cao Vy');
+// });
 
-server.listen(port, hostname, () => {
-    console.log(`Sever running at http://${hostname}:${port}/`);
+// server.listen(port, hostname, () => {
+//     console.log(`Sever running at http://${hostname}:${port}/`);
+// })
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (request, response) => {
+    response.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening in port : ${port}`);
 })
