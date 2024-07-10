310e46283e9d4f209bb85a60ada3d5539a6b90c9
Cao Vy <vihcce170371@fpt.edu.vn>
2024-07-10 19:34:46 +0700

    ADD:: setup EJS

--- src/server.js
+++ src/server.js
@@ -13,11 +13,25 @@
 // })
 
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 8080;
 
+app.set("views", path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (request, response) => {
     response.send('Hello World!')
+})
+
+app.get("/abc", (req, res) => {
+    res.send('Check ABC');
+})
+
+app.get('/ICao', (req, res) => {
+    // res.send(`<h1>I'm Ha Cao Vi</h1>`)
+    res.render('sample.ejs');
 })
 
 app.listen(port, () => {
